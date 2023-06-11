91d039e6c201247c3e357892caad0befc1a99c37
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-06-12 08:20:57 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,148 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
+import theme from "./assets/colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Fontisto } from "@expo/vector-icons";
+
+const STORAGE_KEY = "@toDos";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({}); //많은 사람들이 여기서 array를 쓰는데 우리는 object를 만들거고 hashmap을 사용할거임
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = async (toSave) => {
+    //이 function은 현재 있는 toDos의 object를 string으로 바꾸고 저장할거임
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave)); // setItem을 할 때는 key와 value를 넣어줘야함 @toDos가 key고 그 뒤에 밸류가 들어감 밸류는 string으로만 들어갈 수 있어서 object를 스트링으로 바꿔준 것
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  };
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    //Save To do 나중에 작성할거임
+    /*const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working }, // 이 코드는 Object.assign으로 object와 새로운 object를 합쳐서 리턴하는 코드임 즉, toDos를 깊은 복사를해서 번거롭게 데이터 추가해서 다룰 필요 없이 이걸로 데이터 추가가능
+    });*/
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+  const deleteToDo = async (key) => {
+    Alert.alert("Delete To DO", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm sure",
+        style: "destructive",
+        onPress: () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        },
+      },
+    ]);
+    return;
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onSubmitEditing={addToDo} //사용자가 키보드의 done 버튼을 눌렀을 때 호출되는 함수
+        onChangeText={onChangeText} //text를 입력할 때마다 호출되는 함수
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteToDo(key)}>
+                <Fontisto name="trash" size={18} color={theme.toDoBg} />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnText: {
+    fontSize: 44,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
